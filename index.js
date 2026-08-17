c1ad35704521fd9565860ff5bdf78b0e4d81bb46
Warid-Klabs <warid@byklabs.com>
2026-08-17 20:55:22 +0500

    Added toggle buttons logic

--- index.js
+++ index.js
@@ -11,26 +11,57 @@ const characters = [
 let passwordLengthEl = document.getElementById("passwordLength")
 let password1El = document.getElementById("password1")
 let password2El = document.getElementById("password2")
+let includeSymbolEl = document.getElementById("includeSymbols")
+let includeNumbersEl = document.getElementById("includeNumbers")
+let onlyAlphabets = characters.filter(char => /^[A-Za-z]$/.test(char))
+let symbol_and_alphabets = characters.filter(char => !/^\d$/.test(char));
+let numbers_and_alphabets = characters.filter(char => /^[A-Za-z0-9]$/.test(char));
 
 function generateRandomPassword(){
     let passwordLength = passwordLengthEl.value;
     let randomNumber1 = ""
     let randomNumber2 = ""
+
     for(let i=0; i<passwordLength; i++) {
-        randomNumber1 += characters[getRandomNumber()]
+        if(includeSymbolEl.checked && includeNumbersEl.checked) {
+            randomNumber1 += characters[getRandomNumber(characters.length)]
+
+        } else if (!includeSymbolEl.checked && !includeNumbersEl.checked) {
+            randomNumber1 += onlyAlphabets[getRandomNumber(onlyAlphabets.length)]
+
+        } else if (includeSymbolEl.checked && !includeNumbersEl.checked) {
+            randomNumber1 += symbol_and_alphabets[getRandomNumber(symbol_and_alphabets.length)]
+            
+        } else if (!includeSymbolEl.checked && includeNumbersEl.checked) {
+            randomNumber1 += numbers_and_alphabets[getRandomNumber(numbers_and_alphabets.length)]
+        }
+        
     }  
 
     for(let i=0; i<passwordLength; i++) {
-        randomNumber2 += characters[getRandomNumber()]
-    }
+        if(includeSymbolEl.checked && includeNumbersEl.checked) {
+            randomNumber2 += characters[getRandomNumber(characters.length)]
+
+        } else if (!includeSymbolEl.checked && !includeNumbersEl.checked) {
+            randomNumber2 += onlyAlphabets[getRandomNumber(onlyAlphabets.length)]
+
+        } else if (includeSymbolEl.checked && !includeNumbersEl.checked) {
+            randomNumber2 += symbol_and_alphabets[getRandomNumber(symbol_and_alphabets.length)]
+
+        } else if (!includeSymbolEl.checked && includeNumbersEl.checked) {
+            randomNumber2 += numbers_and_alphabets[getRandomNumber(numbers_and_alphabets.length)]
+        }
+    } 
 
     password1El.textContent = randomNumber1
     password2El.textContent = randomNumber2  
 }
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * characters.length)
+function getRandomNumber(number) {
+    return Math.floor(Math.random() * number)
 }
+
+console.log(getRandomNumber())
 
 function copyPassword(buttonElement) {
     const text = buttonElement.innerText;
